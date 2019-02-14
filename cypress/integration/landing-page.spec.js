94e88961b942e75f6561ef3a466bc146fb2317b0
Matias Garcia <matiasigarcia91@gmail.com>
2019-02-14 10:21:16 +0100

    Add redirection test using cypress

--- cypress/integration/landing-page.spec.js
+++ cypress/integration/landing-page.spec.js
@@ -8,20 +8,51 @@ context("Landing page", () => {
 
     beforeEach(() => {
         cy.login("admin");
+        cy.visit("/");
     });
 
-    it("has page title", () => {
-        cy.title().should("equal", "Vaccination App");
+    describe("when rendered", () => {
+        it("has page title", () => {
+            cy.title().should("equal", "Vaccination App");
+        });
+
+        it("shows 4 pages of the application", () => {
+            cy.get('[data-test="pages"]')
+                .should("have.length", 1)
+                .should("be.visible");
+
+            cy.contains("Campaign Configurator");
+            cy.contains("Data Entry");
+            cy.contains("Dashboard");
+            cy.contains("Maintenance");
+        });
     });
 
-    it("shows 4 pages of the application", () => {
-        cy.get('[data-test="pages"]')
-            .should("have.length", 1)
-            .should("be.visible");
+    describe("when clicked on Campaign Configurator", () => {
+        it("redirects to Campaign Configurator", () => {
+            cy.contains("Campaign Configurator").click();
+            cy.url().should("include", "/campaign-configurator");
+        });
+    });
 
-        cy.contains("Campaign Configurator");
-        cy.contains("Data Entry");
-        cy.contains("Dashboard");
-        cy.contains("Maintenance");
+    describe("when clicked on Data Entry", () => {
+        it("redirects to Data Entry", () => {
+            cy.contains("Data Entry").click();
+            cy.url().should("include", "/data-entry");
+        });
+    });
+
+    describe("when clicked on Dashboard", () => {
+        it("redirects to Dashboard", () => {
+            cy.contains("Dashboard").click();
+            cy.url().should("include", "/dashboard");
+        });
+    });
+
+    describe("when clicked on Maintenance", () => {
+        it("redirects to Maintenance", () => {
+            cy.contains("Maintenance").click();
+            cy.url().should("include", "/maintenance");
+        });
     });
 });
