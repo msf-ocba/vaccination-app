510c01f51d04d2e6b17f857535b719f4cd0fb4b6
Arnau Sanchez <pyarnau@gmail.com>
2019-02-19 14:02:02 +0100

    Move DataTable, ObjectsTable, DetailsBox to d2-ui-components

--- src/models/datasets.js
+++ src/models/datasets.js
@@ -1,9 +1,11 @@
-import _ from "utils/lodash";
+import _ from "lodash";
 
 const fields = [
     "id",
     "name",
+    "code",
     "displayName",
+    "displayDescription",
     "shortName",
     "created",
     "lastUpdated",
@@ -21,16 +23,17 @@ function cleanOptions(options) {
     return _.omitBy(options, value => _.isArray(value) && _.isEmpty(value));
 }
 
-export async function get(d2, filters, pagination) {
-    const { searchValue, showOnlyUserCampaigns } = filters || {};
+export async function list(d2, filters, pagination) {
+    const { search, showOnlyUserCampaigns } = filters || {};
     const { page, pageSize = 20, sorting } = pagination || {};
     // order=FIELD:DIRECTION where direction = "iasc" | "idesc" (insensitive ASC, DESC)
     const [field, direction] = sorting || [];
     const order = field && direction ? `${field}:i${direction}` : undefined;
     const filter = _.compact([
-        searchValue ? `displayName:ilike:${searchValue}` : null,
+        search ? `displayName:ilike:${search}` : null,
         showOnlyUserCampaigns ? `user.id:eq:${d2.currentUser.id}` : null,
     ]);
     const listOptions = { fields, filter, page, pageSize, order };
-    return d2.models.dataSets.list(cleanOptions(listOptions));
+    const collection = await d2.models.dataSets.list(cleanOptions(listOptions));
+    return { pager: collection.pager, objects: collection.toArray() };
 }
