7ee0ba3e054dc2067bc8d072d758726664ca5131
Adrian Quintana <adrianquintana@gmail.com>
2019-04-12 10:17:15 +0100

    pretty that I want you pretty

--- src/components/data-entry/DataEntry.jsx
+++ src/components/data-entry/DataEntry.jsx
@@ -27,10 +27,13 @@ class DataEntry extends React.Component {
 
         if (organisationUnits) {
             let iframe = ReactDOM.findDOMNode(this.refs.iframe);
-            iframe.addEventListener("load", this.setDatasetParameters.bind(this, iframe, dataSetId, organisationUnits));
+            iframe.addEventListener(
+                "load",
+                this.setDatasetParameters.bind(this, iframe, dataSetId, organisationUnits)
+            );
         } else {
             this.props.snackbar.error(i18n.t("Cannot find dataset associated to the campaign"));
-            this.setState({ isDataEntryIdValid: false })
+            this.setState({ isDataEntryIdValid: false });
         }
     }
 
@@ -58,16 +61,14 @@ class DataEntry extends React.Component {
     async setDatasetParameters(iframe, dataSetId, organisationUnits) {
         const iframeDocument = iframe.contentWindow.document;
         this.styleFrame(iframeDocument);
-        
+
         // Select OU in the tree
         const iframeSelection = iframe.contentWindow.selection;
         iframeSelection.select(organisationUnits);
 
         // Wait for OU to be selected and select the dataset
         await this.waitforOUSelection(iframeDocument.querySelector(`#selectedDataSetId`));
-        iframeDocument.querySelector(
-            `#selectedDataSetId [value="${dataSetId}"]`
-        ).selected = true;
+        iframeDocument.querySelector(`#selectedDataSetId [value="${dataSetId}"]`).selected = true;
         iframe.contentWindow.dataSetSelected();
     }
 
@@ -84,12 +85,14 @@ class DataEntry extends React.Component {
                     onBackClick={this.backCampaignConfigurator}
                 />
                 <div>
-                    {isDataEntryIdValid && <iframe
-                        ref="iframe"
-                        title={i18n.t("Data Entry")}
-                        src={"/dhis-web-dataentry/index.action"}
-                        style={styles.iframe}
-                    />}
+                    {isDataEntryIdValid && (
+                        <iframe
+                            ref="iframe"
+                            title={i18n.t("Data Entry")}
+                            src={"/dhis-web-dataentry/index.action"}
+                            style={styles.iframe}
+                        />
+                    )}
                 </div>
             </React.Fragment>
         );
