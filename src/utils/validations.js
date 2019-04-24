17e6ddd528facc894a134f27989ebab403d22db4
Matias Garcia <matiasigarcia91@gmail.com>
2019-04-24 12:09:41 +0200

    Remove : char from validation messages

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -7,7 +7,7 @@ const translations = {
         i18n.t("Only organisation units of level {{levels}} can be selected", namespace),
 
     no_valid_teams_for_organisation_units: namespace =>
-        i18n.t("Organisation Units {{orgUnits}}: no associated teams", namespace),
+        i18n.t("Organisation Units {{orgUnits}} -> no associated teams", namespace),
 
     cannot_be_blank: namespace => i18n.t("Field {{field}} cannot be blank", namespace),
     cannot_be_blank_if_other_set: namespace =>
@@ -27,13 +27,13 @@ const translations = {
 
     age_groups_population_invalid: namespace =>
         i18n.t(
-            "Org unit {{organisationUnit}} has invalid distribution values for age ranges: {{ageGroups}}",
+            "Org unit {{organisationUnit}} has invalid distribution values for age ranges -> {{ageGroups}}",
             namespace
         ),
 
     age_groups_population_for_antigen_invalid: namespace =>
         i18n.t(
-            "Org unit {{organisationUnit}} has an invalid total value for antigen {{antigen}} ({{ageGroups}}): {{- value}}",
+            "Org unit {{organisationUnit}} has an invalid total value for antigen {{antigen}} ({{ageGroups}}) -> {{- value}}",
             namespace
         ),
 };
