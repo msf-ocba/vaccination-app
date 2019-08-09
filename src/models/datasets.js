d86de9ac01523b6d896f546b997ad023b3655a97
Arnau Sanchez <pyarnau@gmail.com>
2019-08-09 11:16:04 +0200

    Refactor datasets.list to remove showOnlyUserCampaigns and add orgUnits filter

--- src/models/datasets.js
+++ src/models/datasets.js
@@ -1,65 +1,86 @@
 import _ from "lodash";
+import { getCurrentUserDataViewOrganisationUnits } from "../utils/dhis2";
 
-const fields = [
+const requiredFields = ["attributeValues[value, attribute[code]]", "organisationUnits[id,path]"];
+
+const defaultListFields = [
     "id",
-    "name",
     "displayName",
     "displayDescription",
-    "shortName",
     "created",
     "lastUpdated",
-    "externalAccess",
     "publicAccess",
     "userAccesses",
     "userGroupAccesses",
     "user",
-    "access",
-    "attributeValues[value, attribute[code]]",
     "dataInputPeriods~paging=(1;1)",
     "href",
 ];
 
-function cleanOptions(options) {
-    return _.omitBy(options, value => _.isArray(value) && _.isEmpty(value));
-}
+/* Return object with pager and array of datasets.
 
+Fields: fields.fields or listFields
+
+Filters:
+
+    - filter.search: string
+    - attributeValues -> RVC_CREATED_BY_VACCINATION_APP === "true"
+    - Orgunits must be in user.dataViewOrganisationUnits.
+
+NOTE: It's not possible to filter all in a single call, so we first make an unpaginated call,
+filter the results, and finally build the pager ({page: number, total: number}) programatically.
+*/
 export async function list(config, d2, filters, pagination) {
-    const { search, showOnlyUserCampaigns } = filters || {};
-    const { page, pageSize = 20, sorting } = pagination || {};
+    const { search, fields: forcedFields } = filters || {};
+    const { page = 1, pageSize = 20, sorting } = pagination || {};
+
     // order=FIELD:DIRECTION where direction = "iasc" | "idesc" (insensitive ASC, DESC)
     const [field, direction] = sorting || [];
     const order = field && direction ? `${field}:i${direction}` : undefined;
-    const vaccinationIds = await getByAttribute(config, d2);
+
     const filter = _.compact([
         search ? `displayName:ilike:${search}` : null,
-        showOnlyUserCampaigns ? `user.id:eq:${d2.currentUser.id}` : null,
-        `id:in:[${vaccinationIds.join(",")}]`,
+        // Preliminar filter for attribute createdByApp (cannot check the value here)
+        `attributeValues.attribute.code:eq:${config.attributeCodeForApp}`,
     ]);
-    const listOptions = { fields, filter, page, pageSize, order };
-    const collection = await d2.models.dataSets.list(cleanOptions(listOptions));
-    return { pager: collection.pager, objects: collection.toArray() };
+    const fields = (forcedFields || defaultListFields).concat(requiredFields).join(",");
+    const listOptions = { fields, filter, pageSize: 1000, order };
+
+    const dataSetsBase = await d2.models.dataSets
+        .list(_.pickBy(listOptions, x => _.isNumber(x) || !_.isEmpty(x)))
+        .then(c => c.toArray());
+
+    const dataSetsFilterd = dataSetsBase.filter(
+        dataSet => isDataSetCreatedByApp(dataSet, config) && isDataSetInUserOrgUnits(d2, dataSet)
+    );
+
+    const dataSetsPaginated = _(dataSetsFilterd)
+        .drop(pageSize * (page - 1))
+        .take(pageSize)
+        .value();
+
+    return {
+        pager: { page, total: dataSetsFilterd.length },
+        objects: dataSetsPaginated,
+    };
 }
 
-async function getByAttribute(config, d2) {
-    const appCode = config.attributeCodeForApp;
-    const filter = `attributeValues.attribute.code:eq:${appCode}`;
-    const listOptions = {
-        filter,
-        fields: ["id", "attributeValues[value, attribute[code]]"],
-        paging: false,
-    };
-    const dataSets = await d2.models.dataSets.list(listOptions);
-    const ids = dataSets
-        .toArray()
-        .filter(
-            ({ attributeValues }) =>
-                !!attributeValues.some(
-                    ({ attribute, value }) =>
-                        attribute && attribute.code === appCode && value === "true"
-                )
-        )
-        .map(el => el.id);
-    return ids;
+function isDataSetCreatedByApp(dataSet, config) {
+    return dataSet.attributeValues.every(
+        attributeValue =>
+            attributeValue.attribute.code !== config.attributeCodeForApp ||
+            attributeValue.value === "true"
+    );
+}
+
+function isDataSetInUserOrgUnits(d2, dataSet) {
+    const userOrgUnits = getCurrentUserDataViewOrganisationUnits(d2);
+
+    return dataSet.organisationUnits.toArray().every(dataSetOrgUnit =>
+        _(dataSetOrgUnit.path.split("/"))
+            .intersection(userOrgUnits)
+            .isNotEmpty()
+    );
 }
 
 export async function getOrganisationUnitsById(id, d2) {
